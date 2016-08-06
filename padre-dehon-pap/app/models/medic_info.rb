4b9f810dbb97667b8e44c12988342decc5a96e0e
Willian <williandricken@gmail.com>
2016-08-06 19:23:13 -0300

    Add medic info to student form

--- padre-dehon-pap/app/models/medic_info.rb
+++ padre-dehon-pap/app/models/medic_info.rb
@@ -2,5 +2,5 @@ class MedicInfo < ActiveRecord::Base
 	has_many :disease_medic_infos
 	has_many :diseases, through: :disease_medic_infos
 
-	belongs_to :student
+	accepts_nested_attributes_for :diseases
 end
