7a35adc22d9efd4763f7ff6e5132880aa848b492
Willian <williandricken@gmail.com>
2016-08-07 13:26:18 -0300

    Add devise and User table

--- padre-dehon-pap/config/routes.rb
+++ padre-dehon-pap/config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :diseases
   resources :responsibles
   resources :behaviors
   resources :students
 
-  get '/', to: 'pages#home'
+  
+
+  root to: "pages#home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
