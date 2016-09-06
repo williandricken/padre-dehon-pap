4927674573f2830d7ee9d98d5b89874b122d5f37
WIllian <willian@ebanx.com>
2016-09-06 18:51:04 -0300

    Add API methods for the campaign flow

--- padre-dehon-pap/app/mailers/concerns/send_grid_mailer.rb
+++ padre-dehon-pap/app/mailers/concerns/send_grid_mailer.rb
@@ -1,9 +1,9 @@
 module SendGridMailer
   include SendGrid
-  # extend ActiveSupport::Concern
-  # def mail (params = {})
-  #   send_email(params)
-  # end
+
+  
+
+  list_id = 553385
 
   def get_content
     Content.new(type: 'text/html', value: get_template)
@@ -13,6 +13,79 @@ module SendGridMailer
     template_id = "9b8ee263-1120-44b6-a287-a01f0dd4ec36"
     template = SendGridMailer.sg.client.templates._(template_id).get()
     JSON.parse(template.body)["versions"].last["html_content"]
+  end
+
+  def get_all_contacts
+    # params = JSON.parse('{"page": 1, "page_size": 1}')
+    response = SendGridMailer.sg.client.contactdb.recipients.get()  #get(query_params: params)
+    puts response.status_code
+    puts response.body
+  end
+
+  def add_contact
+    res = Responsible.last
+    # request = "[
+    #   {
+    #     'email': '#{res.user.email}',
+    #     'first_name': '',
+    #     'last_name': '',
+    #     'responsible_name': '#{res.name}',
+    #     'student_name': '#{res.students.first.name}'
+    #   }"
+    # request = request.to_json
+    data = [{
+      "email": res.user.email,
+      "first_name": "",
+      "last_name": "",
+      "responsible_name": res.name,
+      "student_name": res.students.first.name
+    }].to_json
+    # data2 = JSON.parse(data.to_json)
+    response = SendGridMailer.sg.client.contactdb.recipients.post(request_body: JSON.parse(data))
+    puts response.status_code
+    puts response.body
+  end
+
+  def add_contact_to_list
+    data = JSON.parse('[
+      "c3RheW1hbmx5QGdtYWlsLmNvbQ=="
+    ]')
+    list_id = 559984
+    response = SendGridMailer.sg.client.contactdb.lists._(list_id).recipients.post(request_body: data)
+    puts response.status_code
+    puts response.body
+    puts response.headers
+  end
+
+  def get_all_campaigns
+    #params = JSON.parse('{"limit": 1, "offset": 1}')
+    response = SendGridMailer.sg.client.campaigns.get() #(query_params: params)
+    puts response.status_code
+    puts response.body
+  end
+
+  def send_campaign
+    campaign_id = 480738
+    response = SendGridMailer.sg.client.campaigns._(campaign_id).schedules.now.post()
+    puts response.status_code
+    puts response.body
+  end
+
+  def test_campaign
+    data = JSON.parse('{
+      "to": "willian@ebanx.com"
+    }')
+    campaign_id = 475264
+    response = SendGridMailer.sg.client.campaigns._(campaign_id).schedules.test.post(request_body: data)
+    puts response.status_code
+    puts response.body
+    puts response.headers
+  end
+
+  def get_all_lists
+    response = SendGridMailer.sg.client.contactdb.lists.get()
+    puts response.status_code
+    puts response.body
   end
 
   def self.get_templates
